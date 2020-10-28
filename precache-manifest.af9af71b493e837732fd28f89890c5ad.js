50695ed2c76ede47b7f3f1bd9b60bddd0da4ee3c
i_m.Divyansh <Divyanshbasera36@gmail.com>
2020-10-28 14:43:53 +0530

    Updates

---
+++ precache-manifest.af9af71b493e837732fd28f89890c5ad.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "98048d0353ee153b713602a8eb0b0bbc",
+    "url": "/expensetracker/index.html"
+  },
+  {
+    "revision": "d4ddc9df4c310cde1927",
+    "url": "/expensetracker/static/css/main.86586c16.chunk.css"
+  },
+  {
+    "revision": "6e716f47fcaed37e07b6",
+    "url": "/expensetracker/static/js/2.e20c6916.chunk.js"
+  },
+  {
+    "revision": "65075b20798fa24012a1ad453c78c90d",
+    "url": "/expensetracker/static/js/2.e20c6916.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d4ddc9df4c310cde1927",
+    "url": "/expensetracker/static/js/main.b861ee3f.chunk.js"
+  },
+  {
+    "revision": "319d44707a7d08b6210c",
+    "url": "/expensetracker/static/js/runtime-main.987a9688.js"
+  }
+]);
